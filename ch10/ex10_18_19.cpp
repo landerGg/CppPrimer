b897c48d47dc287abe64b3b63aa440ce325580cf
landerGg <1399126435@qq.com>
2016-01-04 14:37:05 +0800

    Update ex10_18_19.cpp

--- ch10/ex10_18_19.cpp
+++ ch10/ex10_18_19.cpp
@@ -35,6 +35,25 @@ void biggies_partition(std::vector<std::string>& vs, std::size_t sz)
 
     for (auto it = vs.cbegin(); it != pivot; ++it) std::cout << *it << " ";
 }
+//Mine.
+void biggies(vector<string>& words,
+             vector<string>::size_type sz)
+{
+	elimDups(words);
+	stable_sort(words.begin() ,words.end() ,
+	            [] (const string& a,const string& b)
+				   { return a.size()<b.size();});
+	auto wc=std::partition(words.begin() ,words.end() ,
+	               [sz]( const string& a)
+				       { return a.size() >= sz; });
+	            
+    auto count=wc-words.begin() ;
+    cout<<count<<" "<<make_plural(count,"word","s")
+        <<" of length "<<sz<<" or longer "<<endl;
+    for_each(words.begin() ,wc ,
+	        [](const string& s){ cout<<s<<" ";});
+	cout<<endl;
+}
 
 //! ex10.19
 void biggies_stable_partition(std::vector<std::string>& vs, std::size_t sz)
